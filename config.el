2daf13dcc430221452d4a329db4fbc2afd927c23
Yuan Wang <me@yuanwang.ca>
2020-03-16 23:01:58 -0600

    update

--- config.el
+++ config.el
@@ -24,15 +24,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org-note/")
+(setq org-directory "/Users/yuan/org-notes")
 
 
 ;;; :tools direnv
-(setq direnv-always-show-summary nil)
+;;(setq direnv-always-show-summary nil)
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
