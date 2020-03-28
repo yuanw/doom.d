59f5b48f9e6c7288c38c84dbd7686907b42457db
Yuan Wang <me@yuanwang.ca>
2020-03-28 13:44:13 -0600

    add haskell

--- config.el
+++ config.el
@@ -54,3 +54,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+(use-package! lsp-haskell)
+(require 'lsp)
+(require 'lsp-haskell)
+(add-hook 'haskell-mode-hook #'lsp)
+(setq lsp-haskell-process-path-hie "hie-wrapper")
