fc56647454261029bc1833456d38671907551ffd
Yuan Wang <me@yuanwang.ca>
2020-12-26 19:57:10 -0600

    it seems works

--- config.el
+++ config.el
@@ -44,12 +44,16 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org-notes/"
-      org-roam-dailies-directory (concat org-directory "dailies/")
       org-roam-directory (concat org-directory "roam/")
       org-roam-encrypt-files t
+      deft-extensions '("org" "gpg")
+      deft-directory org-directory
       org-agenda-file-regexp "\\`[^.].*\\.org.gpg\\'"
       org-agenda-files (append (file-expand-wildcards (concat org-directory "*.org.gpg"))
                                (file-expand-wildcards (concat org-directory "*.org"))
+                               (file-expand-wildcards (concat org-directory "projects/*.org"))
+                               (file-expand-wildcards (concat org-directory "projects/*.org.gpg"))
+                               (file-expand-wildcards (concat org-directory "Dropbox/project/*.org.gpg"))
                                )
       org-default-notes-file (concat org-directory "Dropbox/project/inbox.org")
       +org-capture-todo-file (concat org-directory "Dropbox/project/inbox.org")
